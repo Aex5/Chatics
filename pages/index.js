d1d69449558b84864938f55d6dc1eef45761bff0
Sujaruu <ytfuga02@gmail.com>
2022-11-21 22:15:19 +0700

    feat: implement login handler

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,43 @@
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import Account from "../components/Account";
+import { useState } from "react";
 
 const Home = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const session = useSession();
   const supabase = useSupabaseClient();
+
+  async function loginHandler() {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+  }
 
   return (
     <div className="container" style={{ padding: "50px 0 100px 0" }}>
       {!session ? (
-        <Auth
-          supabaseClient={supabase}
-          appearance={{ theme: ThemeSupa }}
-          theme="dark"
-          providers={["google", "github"]}
-        />
+        <>
+          <h1>Login</h1>
+          email:
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+          />
+          <br /> Password:
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your Password"
+          />
+          <br />
+          <button onClick={loginHandler}>Login</button>
+          <br />
+        </>
       ) : (
         <Account session={session} />
       )}
